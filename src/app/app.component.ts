742ccdbd6e60068ce8d4ee2b4be184c1208b8141
Kian Gillespie - STUDENT <s00235539@atu.ie>
2024-02-20 15:49:05 +0000

    commit angular project

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Week 2 - To Do List';
+
+  thingsToDo = [
+    "Learn Javascript",
+    "Learn Angular",
+    "Learn Redux"
+  ]
+  thingsCompleted=[];
+
+  summary():string {
+    return '${this.thingsToDo.length} to do / ${this.thingsCompleted.length} done';
+  }
+}
